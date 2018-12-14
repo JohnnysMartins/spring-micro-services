2aebe4f1aac1f9130b7f1991fd9acb3e9f066f15
JohnnysMartins <johnnys.rdsm@gmail.com>
2018-12-14 17:21:29 -0300

    initial commit

---
+++ spring-cloud-config-server/src/main/java/br/com/app/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -0,0 +1,14 @@
+package br.com.app.microservices.springcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+	}
+
+}
+
