a17463cbb7bd23d1ae15af1c282020bb824665b3
JohnnysMartins <johnnys.rdsm@gmail.com>
2019-01-17 17:08:56 -0300

    creating a convertion currency service.

---
+++ currency-conversion-service/src/main/java/br/com/app/microservices/currencyconversionservice/controller/CurrencyConvertionController.java
@@ -0,0 +1,37 @@
+package br.com.app.microservices.currencyconversionservice.controller;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import br.com.app.microservices.currencyconversionservice.dto.CurrencyConvertionDTO;
+
+@RestController
+public class CurrencyConvertionController {
+
+	@GetMapping("/currency-convertion/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConvertionDTO retriveExchange(@PathVariable String from, @PathVariable String to,
+			@PathVariable BigDecimal quantity) throws Exception {
+
+		Map<String, String> uriVariable = new HashMap<>();
+		uriVariable.put("from", from);
+		uriVariable.put("to", to);
+		
+		ResponseEntity<CurrencyConvertionDTO> responseEntity = new RestTemplate().getForEntity(
+				"http://localhost:8000/currency-exchange/from//{from}/to/{to}", CurrencyConvertionDTO.class, uriVariable);
+
+		CurrencyConvertionDTO response = responseEntity.getBody();
+		
+		System.out.println(response.getFrom());
+
+		return new CurrencyConvertionDTO(response.getId(), response.getFrom(), response.getTo(),
+				response.getConversionMultiple(), quantity, quantity.multiply(response.getConversionMultiple()));
+
+	}
+}
